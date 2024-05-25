88eaf50833e170c0702593d94ff4fba764aeda2a
AryanSwami <swamiaryan611@gmail.com>
2024-05-25 16:16:58 +0530

    commit

--- AddingValueInArray.java
+++ AddingValueInArray.java
@@ -1,7 +1,14 @@
 public class AddingValueInArray {
     public static void main(String[] args) {
-        int num[] = { 2, 3, 4, 6, 11 };
+        // int num[] = { 2, 3, 4, 6, 11 };
         SearchingInArray SA = new SearchingInArray();
-        SA.SearchElement(num, 8);
+        // SA.SearchElement(num, 8);
+        int nums[][] = new int[3][3];
+        nums[0][0] = 3;
+        nums[0][1] = 5;
+        nums[1][0] = 6;
+        nums[1][1] = 2;
+        SA.SearchElementIn2dArray(nums, 6);
+
     }
 }
