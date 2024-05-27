ffff88fc5d262ef288ee78c29ece1ffd284038df
AryanSwami <swamiaryan611@gmail.com>
2024-05-27 08:32:18 +0530

    commit

---
+++ noDublicateSortedArray.java
@@ -0,0 +1,16 @@
+public class noDublicateSortedArray {
+    public int removeDuplicates(int[] nums) {
+        // TODO
+        if (nums.length < 1) {
+            return 0;
+        }
+        int index = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+
+}
