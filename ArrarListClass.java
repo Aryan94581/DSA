8b3d79ee96e4b78f60e333912b0a8a2281eb4c1e
AryanSwami <swamiaryan611@gmail.com>
2024-05-28 17:53:04 +0530

    commit

--- ArrarListClass.java
+++ ArrarListClass.java
@@ -2,9 +2,18 @@ import java.util.ArrayList;
 
 public class ArrarListClass {
     public static void main(String[] args) {
-        ArrayList<Integer> number = new ArrayList<Integer>();
-        number.add(1);
-        number.add(2);
+        ArrayList<String> StringList = new ArrayList<String>();
+        StringList.add("A");
+        StringList.add("B");
+        StringList.add("C");
+        StringList.add("D");
+        StringList.add("E");
+        // first way
+        for (int i = 0; i < StringList.size(); i++) {
+            String Value = StringList.get(i);
+            System.out.println(Value);
+        }
+        // Second way
         
     }
 
