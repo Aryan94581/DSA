38f9fab6a3f0bef6580c81ba51e52fa9cb4b351c
AryanSwami <swamiaryan611@gmail.com>
2024-05-28 17:46:46 +0530

    commit

---
+++ ArrarListClass.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArrarListClass {
+    public static void main(String[] args) {
+        ArrayList<Integer> number = new ArrayList<Integer>();
+        number.add(1);
+        number.add(2);
+        System.out.println(number);
+    }
+
+}
