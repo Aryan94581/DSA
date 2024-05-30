006486d7e1a33d99befc33454ee77350d429dbcc
AryanSwami <swamiaryan611@gmail.com>
2024-05-30 16:51:23 +0530

    commit

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        SingleLinkedList sLL = new SingleLinkedList();
-        Node result = sLL.createSingleLinkedList(15);
-        System.out.println(result.value);
+        
     }
 }
