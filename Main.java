f7ecff3181f62051b73f807d0244dda92fff1471
AryanSwami <swamiaryan611@gmail.com>
2024-05-30 17:17:52 +0530

    caommit

--- Main.java
+++ Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         LL list = new LL();
         list.indertFirst(15);
+        list.indertFirst(13);
+        list.indertFirst(17);
+        list.indertFirst(19);
+        list.display();
     }
 }
